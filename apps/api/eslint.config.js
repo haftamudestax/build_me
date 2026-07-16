320b6d7690ed88f1725a1ad40c40f3634892acaa
haftamudesta <desta451616@hotmail.com>
2026-07-16 06:30:32 -0700

    Fix bugs

--- apps/api/eslint.config.js
+++ apps/api/eslint.config.js
@@ -2,7 +2,9 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  {
+    ignores: ["dist/**", ".vercel/**", "node_modules/**", "coverage/**"],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.ts"],
